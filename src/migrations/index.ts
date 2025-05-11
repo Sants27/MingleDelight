3a2801f780a9d0489ed1b502dc0673dc61b0a328
Sant <ignatius.varrel@gmail.com>
2025-05-11 13:37:05 +0700

    chore: remove unused file

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20250502_100552 from './20250502_100552';
-
-export const migrations = [
-  {
-    up: migration_20250502_100552.up,
-    down: migration_20250502_100552.down,
-    name: '20250502_100552'
-  },
-];

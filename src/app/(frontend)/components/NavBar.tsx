15f07e17c1bf5b32a898801c49f3ce8ac0056615
Sant <ignatius.varrel@gmail.com>
2025-05-11 08:25:49 +0700

    refactor(navbar): dynamic logo from Payload global & updated hover colors

--- src/app/(frontend)/components/NavBar.tsx
+++ src/app/(frontend)/components/NavBar.tsx
@@ -1,19 +1,28 @@
 import React from 'react'
 import Link from 'next/link'
-import Image from "next/image";
+import Image from "next/image"
 import Logo from "@/assets/images/logo-circle.png"
+import { getPayload } from 'payload'
+import payloadConfig from '@/payload.config'
+import { Media } from '@/payload-types'
 
-export default function Navbar() {
+export default async function Navbar() {
+  const general = (await getPayload({config: payloadConfig})).findGlobal({
+    slug: "general",
+  });
+
+  const logo = (await general).logo as Media | undefined
+
   return (
     <nav className="flex justify-between items-center py-6 px-8 md:px-16 lg:px-24">
       {/* Logo */}
-      <div className="w-16 h-16">
+      <div className="w-16 h-16 rounded-full overflow-hidden">
         <Image 
-          src={Logo}
-          alt="logo" 
+          src={logo?.url ?? Logo}
+          alt={logo?.alt ?? "Logo"} 
           width={100} 
           height={100}
-          className="w-full h-full" 
+          className="w-full h-full object-cover" 
           priority
         />
       </div>
@@ -24,7 +33,7 @@ export default function Navbar() {
           <Link 
             prefetch 
             href={"/"} 
-            className="hover:text-[#74512D] hover:border-b-2 hover:border-[#74512D] transition-colors"
+            className="text-[#E0C097] hover:text-[#B85C38] hover:border-b-2 hover:border-[#B85C38] transition-colors"
           >
             HOME
           </Link>
@@ -33,7 +42,7 @@ export default function Navbar() {
           <Link 
             prefetch 
             href={"/about"} 
-            className="hover:text-[#74512D] hover:border-b-2 hover:border-[#74512D] transition-colors"
+            className="text-[#E0C097] hover:text-[#B85C38] hover:border-b-2 hover:border-[#B85C38] transition-colors"
           >
             ABOUT
           </Link>
@@ -42,7 +51,7 @@ export default function Navbar() {
           <Link 
             prefetch 
             href={"/menu"} 
-            className="hover:text-[#74512D] hover:border-b-2 hover:border-[#74512D] transition-colors"
+            className="text-[#E0C097] hover:text-[#B85C38] hover:border-b-2 hover:border-[#B85C38] transition-colors"
           >
             MENU
           </Link>
@@ -51,7 +60,7 @@ export default function Navbar() {
           <Link 
             prefetch 
             href={"/news"} 
-            className="hover:text-[#74512D] hover:border-b-2 hover:border-[#74512D] transition-colors"
+            className="text-[#E0C097] hover:text-[#B85C38] hover:border-b-2 hover:border-[#B85C38] transition-colors"
           >
             NEWS
           </Link>
